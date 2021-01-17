8e138b3668ba7a4b2b25982636f4cb0ba5e033c2
Himanshu MacTavish <52399098+h-mactavish@users.noreply.github.com>
2021-01-17 16:11:32 +0530

    Added Pattern.java
    Identifying pattern source code

---
+++ Pattern.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Pattern{
+
+
+    public static void main(final String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        sc.close();
+        int min=0;
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=n;j++){
+                min=i<j?i:j;
+                System.out.print(n-min+1);
+            }
+
+            for(int j=n-1;j>=1;j--){
+                min=i<j?i:j;
+                System.out.print(n-min+1);
+            }
+            System.out.println();
+        }
+
+        for(int i)
+        
+    }
+}
